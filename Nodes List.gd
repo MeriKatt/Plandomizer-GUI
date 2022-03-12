40cc9863156b28cc1cd3246c0697d3b6fd0eef14
MeriKatt <devtanukikitamura@gmail.com>
2022-03-12 01:53:05 -0600

    Create tab area and world selection basis

---
+++ Nodes List.gd
@@ -0,0 +1,20 @@
+extends ItemList
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Room_item_selected(index):
+	pass # Replace with function body.
---
+++ Room Node Selection.gd
@@ -0,0 +1,43 @@
+extends OptionButton
+
+func DePrint(obj):
+	 Globals.DePrint(obj)
+
+var worlds = Globals.worlds
+
+var elevators_world = Globals.elevators_world
+var elevators_room = Globals.elevators_room
+var current_world = null
+var current_room = null
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var i = 1
+	var array = Globals.get_array("Frigate Orpheon")
+	for x in array:
+		add_item(x,i)
+		i+=1
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area_item_selected(index):
+	if worlds[index] == "":
+		return
+	current_world = worlds[index]
+	clear()
+	var i = 1
+	var array = Globals.get_array(current_world)
+	for x in array:
+		add_item(x,i)
+		i+=1
+
+
+func _on_Room_item_selected(index):
+	current_room = Globals.get_array(current_world)[index]
+	DePrint(current_room)
